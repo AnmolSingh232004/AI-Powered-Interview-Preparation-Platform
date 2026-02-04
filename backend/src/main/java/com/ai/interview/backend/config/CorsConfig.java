ab4181eea9daa621e8585d45bda35c856d434904
AnmolSingh232004 <anmol.singh232004@gmail.com>
2026-02-05 01:38:06 +0530

    Cors Config Allowing Vercel

--- backend/src/main/java/com/ai/interview/backend/config/CorsConfig.java
+++ backend/src/main/java/com/ai/interview/backend/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://ai-powerd-interview-prep.vercel.app"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
